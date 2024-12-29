28b34843c41a35ced929f7c3d0713f48383c5695
ceusahin <hakonsahin@gmail.com>
2024-12-30 00:01:09 +0300

    update responsive design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,28 +6,28 @@ export default function Main() {
   const { lang } = useContext(LangContext);
 
   return (
-    <div className="mx-40 max-500:overflow-x-hidden max-500:w-full">
-      <div className="flex flex-row items-center mt-16 mb-9 max-500:mb-16">
+    <div className="mx-40 max-500:overflow-x-hidden max-500:mx-6 max-500:w-full">
+      <div className="flex flex-row items-center mt-16 mb-9 max-500:mb-16 max-500:mt-10 max-500:w-[22rem] max-500:mx-auto">
         <div className="border-r-[100px] border border-[#4338ca] dark:border-[#B7AAFF]"></div>
         <span className="text-2xl text-[#4338ca] ml-4 dark:text-[#B7AAFF]">
           Hakan
         </span>
       </div>
-      <main className="flex justify-between">
-        <section className="w-[60rem]">
-          <h1 className="font-bold text-8xl w-[55rem] dark:text-[#AEBCCF] max-500:text-5xl">
+      <main className="flex justify-between max-500:flex-col-reverse">
+        <section className="w-[60rem] max-500:w-full">
+          <h1 className="font-bold text-8xl w-[55rem] dark:text-[#AEBCCF] max-500:text-5xl max-500:w-[22rem] max-500:text-center">
             {lang == "tr" ? "Özgün Düşünce" : "Original Ideas"}
           </h1>
           <br />
-          <h1 className="font-bold text-8xl w-[55rem] dark:text-[#AEBCCF] max-500:text-5xl">
+          <h1 className="font-bold text-8xl w-[55rem] dark:text-[#AEBCCF] max-500:text-5xl max-500:w-[22rem] max-500:text-center">
             {lang == "tr" ? "Etkili Çözümler" : "Clear Results"}
           </h1>
-          <p className="w-[46rem] my-12 text-2xl dark:text-white max-500:w-[27rem]">
+          <p className="w-[46rem] my-12 text-2xl dark:text-white max-500:w-[22rem] max-500:text-center">
             {lang == "tr"
               ? "Merhaba, ben Hakan. Full-stack yazılım geliştiricisiyim. Eğer sağlam ve özelleştirilebilir frontend ürünleri tasarlayıp, harika kullanıcı deneyimleri sunabilecek bir geliştirici arıyorsanız, gelin el sıkışalım!"
               : "  Hi, I'm Hakan. I'm a full-stack software developer. If you are looking for a Developer who to craft solid and scalable frontend products with great user experiences. Lets shake hands with me."}
           </p>
-          <nav className="flex items-center max-500:flex-wrap max-500:w-[30rem]">
+          <nav className="flex items-center max-500:flex-wrap max-500:w-[22rem] max-500:mx-0">
             <Link
               to="/contact"
               className="max-500:mt-8 max-500:mr-0 max-500:mb-6 w-[14rem] text-center mr-6 border border-[#3730A3] px-12 py-4 text-white bg-[#3730A3] text-2xl rounded-2xl dark:bg-[#E1E1FF] dark:border-none dark:text-black max-500:w-[26rem]"
@@ -36,7 +36,7 @@ export default function Main() {
             </Link>
             <a
               href="https://github.com/ceusahin"
-              className="text-[#3730A3] mr-6  border-[#3730A3] px-9 py-4 border text-2xl rounded-2xl dark:bg-[#383838] dark:border-[#E1E1FF] dark:text-[#E1E1FF]"
+              className="text-[#3730A3] max-500:w-[26rem] max-500:mx-auto mr-6  border-[#3730A3] px-9 py-4 border text-2xl rounded-2xl dark:bg-[#383838] dark:border-[#E1E1FF] dark:text-[#E1E1FF]"
               target="_blank"
             >
               <button className="flex items-center ">
@@ -47,7 +47,7 @@ export default function Main() {
             <a
               target="_blank"
               href="https://www.linkedin.com/in/hakonsahin/"
-              className="text-[#3730A3] mr-14 max-500:mr-0 border-[#3730A3] px-9 py-4 border text-2xl rounded-2xl dark:bg-[#383838] dark:border-[#E1E1FF] dark:text-[#E1E1FF] max-500:w-[12.5rem]"
+              className="text-[#3730A3] max-500:mt-6 max-500:w-[26rem] max-500:mx-auto mr-14 max-500:mr-0 border-[#3730A3] px-9 py-4 border text-2xl rounded-2xl dark:bg-[#383838] dark:border-[#E1E1FF] dark:text-[#E1E1FF]"
             >
               <button className="flex items-center">
                 <i className="fa-brands fa-linkedin-in fa-xl pr-3" />
@@ -58,7 +58,7 @@ export default function Main() {
         </section>
         <section className="">
           <img
-            className="object-contain rounded-l-3xl w-full h-[28rem]"
+            className="object-contain rounded-l-3xl w-full h-[28rem] max-500:w-[22rem] max-500:mx-2"
             src="https://picsum.photos/700/500"
             alt=""
           />
